3d7718b986cb0a1188a2ee88daa90116b4771014
Radford98 <powelbra@oregonstate.edu>
2019-04-26 21:24:17 -0700

    Add randomize rooms function

--- powelbra.buildrooms.c
+++ powelbra.buildrooms.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
 
 struct room {
 	int id;
@@ -28,8 +30,12 @@ int CanAddConnectionFrom(struct room);
 int ConnectionAlreadyExists(struct room, struct room);
 void ConnectRoom(struct room, struct room);
 int IsSameRoom(struct room, struct room);
+void RandomizeRooms(char**);
 
 int main() {
+	// Create a random seed based on the time
+	srand(time(0));
+
 	// Create names for rooms
 	char* roomNames[10];
 	roomNames[0] = "Foyer";
@@ -43,13 +49,35 @@ int main() {
 	roomNames[8] = "Armory";
 	roomNames[9] = "Chapel";
 
-	printf("Hello!\n");
-
-
+	// Randomize the room names for assignment.
+	RandomizeRooms(roomNames);
 
 
 	return 0;
 }
+
+// Function which takes the array of room names and randomizes them. I remember learning this
+// technique in a previous class, but not the name of the technique. It is similar to a heap
+// sort in that a randomly chosen index is swapped with the last index, then the max number
+// for rand is decreased by one so previously selected names aren't selected again.
+// After the whole thing runs, the 7 chosen names will be in indexes [3]-[9].
+void RandomizeRooms(char** array) {
+	int select;	// variable to hold the randomly selected index
+	char* temp;	// variable to temporary hold names for swapping
+	int i;
+
+	// Loop 7 times, moving the selected name to the back of the array
+	// where it won't be touched by a decreasing rand 'max'.
+	for (i = 0; i < 7; i++) {
+		// rand() % 10 gives number 0-9. By subtracting i each iteration,
+		// the remaining names is shortened.
+		select = rand() % (10-i);
+		temp = array[select];
+		array[select] = array[9-i];	// Last index (9, 8, 7....)
+		array[9-i] = temp;
+	}	
+}
+
 
 // Create all connections in the graph. The loop checks if the graph is full, and
 // if it isn't, add a new connection between rooms.
