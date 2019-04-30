c0d587efd771d3043cf106b3eca116262932406d
Radford98 <powelbra@oregonstate.edu>
2019-04-29 21:31:18 -0700

    Open and read newest room dir; figured out how to read data from file

--- powelbra.adventure.c
+++ powelbra.adventure.c
@@ -33,7 +33,79 @@ void CreateRooms(struct room[]);
 int main() {
 	// Create an array of structs for holding the rooms
 	struct room rooms[7];
-
+	// Find the newest directory and create the rooms
+	NewestDir(rooms);
 
 	return 0;
 }
+
+// Function which accepts an array of 7 room structs. It finds the newest directory with a given prefix
+// and pulls the files out of the directory to create room structs for the array.
+// Uses the code from 2.4 Manipulating Directories as a model.
+void NewestDir(struct room rooms[]) {
+	int newestDirTime = -1;				// Variable to hold and compare timestamps of directories
+	char targetDirPrefix[20] = "powelbra.rooms.";	// Prefix of directories to search
+	char newestDirName[256];			// Holds the name of the newest directory found
+	memset(newestDirName, '\0', sizeof(newestDirName));
+
+	DIR* dirToCheck;	// Holds the directory to search, namely, the current directory
+	struct dirent *fileInDir;	// Holds the files/folders in the current directory for checkings
+	struct stat dirAtt;	// Holds information about the subdir
+
+	dirToCheck = opendir(".");	// Open the current directory
+
+	if (dirToCheck > 0) {	// If the directory opened successfully
+		while ((fileInDir = readdir(dirToCheck)) != NULL) {	// Check each entry in dirToCheck
+			if (strstr(fileInDir->d_name, targetDirPrefix) != NULL) { // If entry has the prefix
+				stat(fileInDir->d_name, &dirAtt);	// Get attributes of the entry
+
+				if ((int)dirAtt.st_mtime > newestDirTime) {	// If this time is more recent
+					newestDirTime = (int)dirAtt.st_mtime;
+					memset(newestDirName, '\0', sizeof(newestDirName));	// Reset string
+					strcpy(newestDirName, fileInDir->d_name);	// Set to newest dir
+				}
+			}
+		}
+	}
+	closedir(dirToCheck);	// After searching, close the current directory
+	dirToCheck = opendir(newestDirName);	// Open the newest directory
+
+	if (dirToCheck > 0) {
+		while ((fileInDir = readdir(dirToCheck)) != NULL) {
+			if (strlen(fileInDir->d_name) > 2){	// Ignore . and .. directories
+				char fileName[50];
+				memset(fileName, '\0', sizeof(fileName));
+				sprintf(fileName, "%s/%s", newestDirName, fileInDir->d_name);
+				FILE* roomFile = fopen(fileName, "r");
+
+				// Print room name
+				char fileLine[50];
+				memset(fileLine, '\0', sizeof(fileLine));
+				fscanf(roomFile, "%*s %*s %s", fileLine);
+				printf("%s\n", fileLine);
+
+				// Check connections, set fileLine to connection to start
+				fscanf(roomFile, "%s", fileLine);
+				while (strcmp("CONNECTION", fileLine) == 0) {	// If the next line is CONNECTION
+					char line[10];
+					fscanf(roomFile, "%s %s", line, fileLine);
+					printf("Connection %s %s\n", line, fileLine);
+					fscanf(roomFile, "%s", fileLine);	// Set fileLine to next line
+				}
+				fscanf(roomFile, "%*s %s", fileLine);	// skip "type" and save name
+				printf("Room type: %s\n", fileLine);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
